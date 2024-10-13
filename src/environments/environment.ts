5c45d99df2c7463d168338852277e389c67aadbf
PaulPPS632 <jeffertsap@hotmail.com>
2024-10-13 09:41:26 -0500

    change ruta api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //API_URL: 'https://aybecoconsultingperu.com/api',
   //API_URL: 'http://localhost:3000/api',
-  API_URL: 'http://localhost:3000/api2',
+  API_URL: 'https://aybecoconsultingperu.com/api2',
 };
